b6e6cadb810325d3e7e87ae67455c92f699c7d8b
原昂大 <takahiro.hara@ietty.co.jp>
2020-01-20 21:07:48 +0900

    スクロールを検知してセルを追加する処理を実装

--- ScrollSample/ViewController.swift
+++ ScrollSample/ViewController.swift
@@ -12,16 +12,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var tableView: UITableView!
 
-    var count: Int = 5
+    var num: Int = 5
+    
+    // addCellsメソッドの実行回数をカウントするための変数
+    var executionsCount: Int = 0
+    
+    var addCount: Int = 0
+    
+    var isFetching: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    func addCells() {
+        executionsCount += 1
+        print("addCellsメソッドを実行しました。 \(executionsCount)回目")
+        
+        if isFetching { return }
+        
+        isFetching = true
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.num += 10
+            self.tableView.reloadData()
+            self.isFetching = false
+            
+            self.addCount += 1
+            print("実際にデータを追加した回数 \(self.addCount)回")
+        }
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return count
+        return num
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -31,7 +56,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return 100
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let currentOffsetY: CGFloat = scrollView.contentOffset.y
+        let maximumOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let distanceToBottom: CGFloat = maximumOffset - currentOffsetY
+        
+        if !tableView.isTracking {
+            if distanceToBottom < 1000 {
+                addCells()
+            }
+        }
+    }
 }
